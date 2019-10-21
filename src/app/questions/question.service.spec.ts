7a0706d00d5803b28863561148505dd5cf012382
twong540 <twong@reliaslearning.com>
2019-10-21 15:58:08 -0400

    QuestionServiceSpec Fix

--- src/app/questions/question.service.spec.ts
+++ src/app/questions/question.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
-import { QuestionService } from './question-service.service';
+import { QuestionService } from './question.service';
 
-describe('QuestionServiceService', () => {
+describe('QuestionService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
