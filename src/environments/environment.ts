5b2c7d08eca74098d698b844b3f848a6b86ab7e8
James Bruening <jwbruening@gmail.com>
2019-10-23 11:35:03 -0400

    updated routing and environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
   production: false,
   environment: 'LOCAL',
-  apiURL: 'https://localhost:5001/interview/'
+  apiURL: 'https://reliasinterviewapi.azurewebsites.net/interview/'
 };
