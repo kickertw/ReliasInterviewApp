238040923d3f4d1faa5670b4155c171da7d2a668
twong540 <twong@reliaslearning.com>
2019-10-24 10:30:54 -0400

    TSLint fixes

--- src/app/test-player/test-player.component.ts
+++ src/app/test-player/test-player.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { TestPlayerService } from './test-player.service';
-import { QuestionService } from '../questions/question.service';
-import { TestService } from '../test/test.service';
-import { Question } from '../questions/shared/models/question.model';
-import { Response } from './shared/models/response.model';
-import { MatSort, MatTableDataSource } from '@angular/material';
-import { Router, ActivatedRoute } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { TestPlayerService } from "./test-player.service";
+import { QuestionService } from "../questions/question.service";
+import { TestService } from "../test/test.service";
+import { Question } from "../questions/shared/models/question.model";
+import { Response } from "./shared/models/response.model";
+import { MatSort, MatTableDataSource } from "@angular/material";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-test-player',
-  templateUrl: './test-player.component.html',
-  styleUrls: ['./test-player.component.scss']
+  selector: "app-test-player",
+  templateUrl: "./test-player.component.html",
+  styleUrls: ["./test-player.component.scss"]
 })
 export class TestPlayerComponent implements OnInit {
-  displayedColumns: string[] = ['Id', 'Question', 'Type', 'Level'];
+  displayedColumns: string[] = ["Id", "Question", "Type", "Level"];
   dataSource: MatTableDataSource<Question>;
   questions: Array<Question>;
   responses: Array<Response>;
@@ -26,7 +25,8 @@ export class TestPlayerComponent implements OnInit {
     private route: ActivatedRoute,
     private testPlayerService: TestPlayerService,
     private questionService: QuestionService,
-    private testService: TestService) { }
+    private testService: TestService
+  ) {}
 
   ngOnInit() {
     this.buildTest();
@@ -47,16 +47,20 @@ export class TestPlayerComponent implements OnInit {
   }
 
   saveResponse() {
-    if (this.response.answer === null){
-      this.testPlayerService.createResponse(this.response).subscribe(
-        response => console.log(response),
-        error => console.log(error)
-      );
+    if (this.response.answer === null) {
+      this.testPlayerService
+        .createResponse(this.response)
+        .subscribe(
+          response => console.log(response),
+          error => console.log(error)
+        );
     } else {
-      this.testPlayerService.updateResponse(this.response).subscribe(
-        response => console.log(response),
-        error => console.log(error)
-      );
+      this.testPlayerService
+        .updateResponse(this.response)
+        .subscribe(
+          response => console.log(response),
+          error => console.log(error)
+        );
     }
   }
 }
